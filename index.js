aa797ea3909ce907e98c8a5fad3c34b75b725bd2
matt22881 <matt22881@gmail.com>
2021-08-01 22:07:12 -0400

    write migrations

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // start your server here
+require('dotenv').config()
+const server = require('./api/server')
+
+const p = 5000
+
+
+server.listen(p => {
+    console.log(`\n --- API Server listening on port ${p} --- \n`)
+})
